daeefa8cb8b137d3fabd6f7ff96ddfd859fe7001
Chandrashekhar gowda KS <106522596+Chandra583@users.noreply.github.com>
2023-02-03 22:58:31 +0530

    work.js_changed

--- frontend2/src/container/Work/Work.jsx
+++ frontend2/src/container/Work/Work.jsx
@@ -44,7 +44,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['FrontEnd','React JS', 'Mobile App','Web3', 'All'].map((item, index) => (
+        {['React JS','FrontEnd', 'Mobile App','Web3', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
