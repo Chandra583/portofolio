79f5b9a301bc555a10e2a9bf394d1487a443f275
Chandrashekhar gowda KS <106522596+Chandra583@users.noreply.github.com>
2023-02-04 00:21:30 +0530

    new

--- frontend2/src/container/Header/Header.jsx
+++ frontend2/src/container/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="h-text">Hello, I am</p>
-            <h1 className="head-text">ChandraShekar</h1>
+            <h1 className="head-text">Chandra</h1>
           </div>
         </div>
 
